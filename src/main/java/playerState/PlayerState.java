19e43f8e71f855a48f01bd1a937c588ed120fdff
Morariu Dorian Sebastian <dorian_tm@yahoo.com>
2020-05-16 16:31:51 +0300

    WGA-22 Added Player State GUI

---
+++ src/main/java/playerState/PlayerState.java
@@ -0,0 +1,11 @@
+package playerState;
+
+import javax.swing.*;
+
+public class PlayerState {
+    private JFrame window=new JFrame("Player State"); //to put player name in title
+    private static void initGUI()
+    {
+
+    }
+}
