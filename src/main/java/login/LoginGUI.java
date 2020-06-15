7e0fbd9151882dddfb6e6e9c577c505f10b3c6a9
Daniel Cernaianu <daniel.cernaianu@student.upt.ro>
2020-06-15 23:40:43 +0300

    WGA-34 Created admin after login GUI with access to features

--- src/main/java/login/LoginGUI.java
+++ src/main/java/login/LoginGUI.java
@@ -9,7 +9,7 @@ public class LoginGUI extends JFrame{
     private JButton buttonLogin = new JButton("Login");
     private JLabel labelUser = new JLabel("Username");
     private JLabel labelPass = new JLabel("Password");
-    private JLabel labelTitle = new JLabel(("Wargame"));
+    private JLabel labelTitle = new JLabel("Wargame");
     private JTextField fieldUser = new JTextField();
     private JPasswordField fieldPass = new JPasswordField();
     private JOptionPane jopt = new JOptionPane();
